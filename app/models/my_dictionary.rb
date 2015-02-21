8fb62063804e2921b7bf1ff2e35dec5bedc4fe4a
klimmen <klim030187@gmail.com>
2015-02-22 00:46:39 +0200

    add a column 'counter' in the table lessons

--- app/models/my_dictionary.rb
+++ app/models/my_dictionary.rb
@@ -1,5 +1,5 @@
 class MyDictionary < ActiveRecord::Base
-  belongs_to :lesson
+  belongs_to :lesson, dependent: :destroy, counter_cache: true
   validates :eng, presence: true
   validates :rus, presence: true
   validates :lesson_id, presence: true
